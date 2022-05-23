87d7c167b96f038848573166130a06a23cdf3071
Vadim <103464053+Branios@users.noreply.github.com>
2022-05-24 01:44:47 +0300

    design Login page

--- About me/ViewControllers/LoginViewController.swift
+++ About me/ViewControllers/LoginViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
 
 }
 
