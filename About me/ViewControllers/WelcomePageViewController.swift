575e7d9150a710929579edb36b59e1c9dde14f25
Vadim <103464053+Branios@users.noreply.github.com>
2022-05-24 02:38:02 +0300

    set design constraints for login page and welcome page

---
+++ About me/ViewControllers/WelcomePageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePageViewController.swift
+//  About me
+//
+//  Created by VK on 24.05.2022.
+//
+
+import UIKit
+
+class WelcomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
