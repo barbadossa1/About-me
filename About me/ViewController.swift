049088fa17922f7b4306e20f179937f3f2a60491
Vadim <103464053+Branios@users.noreply.github.com>
2022-05-24 00:29:47 +0300

    Initial Commit

---
+++ About me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About me
+//
+//  Created by VK on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
