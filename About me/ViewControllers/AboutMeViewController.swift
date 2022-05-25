8e532bb277b8b31006aee73cd32b985de9b11571
Vadim <103464053+Branios@users.noreply.github.com>
2022-05-26 02:53:51 +0300

    add new ViewControllers for new screens and add info about me

---
+++ About me/ViewControllers/AboutMeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeViewController.swift
+//  About me
+//
+//  Created by VK on 26.05.2022.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ About me/ViewControllers/MyHobbyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MyHobbyViewController.swift
+//  About me
+//
+//  Created by VK on 26.05.2022.
+//
+
+import UIKit
+
+class MyHobbyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
