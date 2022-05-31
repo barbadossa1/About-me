3263436c119ac7430cf9e4bddbe7dfe3076b6afa
Vadim <103464053+Branios@users.noreply.github.com>
2022-06-01 02:20:00 +0300

    added  a user info description to the model

--- About me/Models/User.swift
+++ About me/Models/User.swift
@@ -35,8 +35,12 @@ struct Person {
         Person(
             name: "Vadim",
             surname: "Kuznetsov",
-            aboutDescription: "ergqergqerg",
-            hobbyDescription: "wfbfbwbw",
+            aboutDescription: """
+            Hi! My name is Vadim. For the last 6 years I have been working in a large e-commerce company as a project manager. He worked mainly with large Western companies. Participated in the development of 7 branded online stores and supported them. Now it's time to switch to development
+            """,
+            hobbyDescription: """
+            I continue to help companies create businesses in the field of e-commerce, but most of the time I am engaged in teaching the Swift programming language.
+            """,
             image: "myPhoto"
         )
     }
